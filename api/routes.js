3661cbf3efe09e49f4658de5e5980361a5b65966
hellohrm <61720510+hellohrm@users.noreply.github.com>
2020-08-20 22:43:46 +0700

    Add files via upload

--- api/routes.js
+++ api/routes.js
@@ -41,21 +41,24 @@ module.exports = function (app) {
 
     // generic function sending messages
     function sendMessage(recipientId, message) {
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-            method: 'POST',
-            json: {
-                recipient: { id: recipientId },
-                message: message,
-            }
-        }, function (error, response, body) {
-            if (error) {
-                console.log('Error sending message: ', error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            }
-        });
+        console.log('access_token: ', process.env.PAGE_ACCESS_TOKEN);
+        console.log('recipientId: ', recipientId);
+        console.log('message: ', message);
+        //request({
+        //    url: 'https://graph.facebook.com/v2.6/me/messages',
+        //    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        //    method: 'POST',
+        //    json: {
+        //        recipient: { id: recipientId },
+        //        message: message,
+        //    }
+        //}, function (error, response, body) {
+        //    if (error) {
+        //        console.log('Error sending message: ', error);
+        //    } else if (response.body.error) {
+        //        console.log('Error: ', response.body.error);
+        //    }
+        //});
     };
 
 };
