7c595c148cf090d0ae0243641a27c71d62c1c3ef
caicangcua <36462470+caicangcua@users.noreply.github.com>
2020-09-02 16:09:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 
+const WEBHOOK_SECRET = '8a25cff620291e86fc6c81755ee3b8c78268902623a610d42464c1724f78ee352bd59d237e5747e544aae06d2b965864';
+const crypto = require('crypto');
+function verifySignature(body, signature) {
+    const digest = crypto
+        .createHmac('sha1', WEBHOOK_SECRET)
+        .update(body)
+        .digest('hex');
+    return signature === digest;
+};
+app.post('/webhooks', function (req, res, next) {
+    if (!verifySignature(req.rawBody, req.headers['x-tawk-signature'])) {
+            //verification failed
+        console.log('verification failed: ', req.rawBody);
+    }
+
+    console.log('verification success: ', req.rawBody);
+});
+
 let routes = require('./api/routes') //importing route
 routes(app)
 
