53d153ea7492b1e33fa501c3858637ec3953bc20
caicangcua <36462470+caicangcua@users.noreply.github.com>
2020-09-02 15:27:24 +0700

    Update routes.js

--- api/routes.js
+++ api/routes.js
@@ -63,4 +63,23 @@ module.exports = function (app) {
         });
     };
 
+    //https://developer.tawk.to/webhooks/
+    const WEBHOOK_SECRET = 'tawk.to-webhook-secret-key';
+    const crypto = require('crypto');
+    function verifySignature(body, signature) {
+        const digest = crypto
+            .createHmac('sha1', WEBHOOK_SECRET)
+            .update(body)
+            .digest('hex');
+        return signature === digest;
+    };
+    app.post('/webhooks', function (req, res, next) {
+        if (!verifySignature(req.rawBody, req.headers['x-tawk-signature'])) {
+            // verification failed
+            console.log('verification failed: ', req.rawBody);
+        }
+        // 
+        console.log('verification success: ', req.rawBody);
+    });
+    //--------------------------------------------------------------------
 };
