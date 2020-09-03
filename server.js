a1a4f767f229d6faf97cc88a32b15d75ecf36269
caicangcua <36462470+caicangcua@users.noreply.github.com>
2020-09-03 09:10:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ function startKeepAlive() {
         }).on('error', function (err) {
             console.log("keepalive error: " + err.message);
         });
-    }, 1 * 60 * 1000); // load every 20 minutes
+    }, 5 * 60 * 1000); // load every 20 minutes
 }
 startKeepAlive();
 //END PREVENT HEROKU SLEEP ....................
