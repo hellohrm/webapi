fa346071092ce0fde8ffa0b1ede2c8f525f8f2e4
caicangcua <36462470+caicangcua@users.noreply.github.com>
2020-09-23 14:13:59 +0700

    v5

--- server.js
+++ server.js
@@ -176,10 +176,11 @@ routes(app)
 
 app.use(function(req, res) {
     //res.status(404).send({ url: req.originalUrl + ' not found' })
-    res.writeHead(301,
-        { Location: 'https://hrpro.cf/pages/invoice.html' } //'https://192.168.1.91:10996/pages/invoice.html'
-    );
-    res.end();
+    res.status(301).redirect('https://hrpro.cf/pages/invoice.html' + req.url)
+    //res.writeHead(301,
+    //    { Location: 'https://hrpro.cf/pages/invoice.html' } //'https://192.168.1.91:10996/pages/invoice.html'
+    //);
+    //res.end();
 })
 
 app.listen(port)
