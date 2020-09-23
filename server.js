43e3f1fe6970436f3279ee97711f185a2df6953a
caicangcua <36462470+caicangcua@users.noreply.github.com>
2020-09-23 11:02:28 +0700

    vs

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 //
 //
 //START PREVENT HEROKU SLEEP ....................
-var https = require('https');
+var https = require('http');
 app.route('/keepalive').get(function (req, res) {
     var d = new Date();
     var options = {
@@ -21,20 +21,109 @@ app.route('/keepalive').get(function (req, res) {
     };
     res.send(d.toLocaleString("vi-vn"));
 });
+
+
 function startKeepAlive() {
     setInterval(function () {
-        var req = https.get("https://hrpro.cf/localsrc/ping.php", function (res) {//"https://hellohrmapi.herokuapp.com/keepalive"
-            res.on('data', function (chunk) {
-                try {
-                    // optional logging... disable after it's working
-                    console.log("HEROKU INVOKE OK: " + chunk);
-                } catch (err) {
-                    console.log("HEROKU INVOKE ERROR: " + err.message);
+        //////////var req = https.get("https://hrpro.cf/localsrc/ping.php", function (res) {//"https://hellohrmapi.herokuapp.com/keepalive"
+        //////////    res.on('data', function (chunk) {
+        //////////        try {
+        //////////            // optional logging... disable after it's working
+        //////////            console.log("HEROKU INVOKE OK: " + chunk);
+        //////////        } catch (err) {
+        //////////            console.log("HEROKU INVOKE ERROR: " + err.message);
+        //////////        }
+        //////////    });
+        //////////}).on('error', function (err) {
+        //////////    console.log("keepalive error: " + err.message);
+        //////////});
+
+        ////////try {
+        ////////    var querystring = require('querystring');
+        ////////    var postData = querystring.stringify({
+        ////////        'msg': 'Hello World!'
+        ////////    });
+
+            var options = {
+                hostname: 'hrpro.cf',
+                port: 443,
+                path: '/localsrc/ping.php',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': Buffer.byteLength(postData)
                 }
+            };
+
+            var req = https.request(options, (res) => {
+                console.log('STATUS:', res.statusCode);
+                console.log('HEADERS:', JSON.stringify(res.headers));
+                res.setEncoding('utf8');
+                res.on('data', (chunk) => {
+                    console.log('BODY:',chunk);
+                });
+                res.on('end', () => {
+                    console.log('No more data in response.');
+                });
             });
-        }).on('error', function (err) {
-            console.log("keepalive error: " + err.message);
-        });
+
+            req.on('error', (e) => {
+                console.log(`problem with request: ${e.message}`);
+            });
+
+            // write data to request body
+            req.write(postData);
+            req.end();
+        } catch (err) {
+            console.log("HEROKU INVOKE ERROR: " + err.message);
+        }
+
+
+
+        //////try {
+        //////    var jsonObject = JSON.stringify({
+        //////        'Value1': 'abc1',
+        //////        'Value2': 'abc2',
+        //////        'Value3': '3'
+        //////    });
+
+        //////    var options = {
+        //////        hostname: '192.168.1.91',
+        //////        port: 10996,
+        //////        path: '/localsrc/ping.php?XDEBUG_SESSION_START=154A5348',
+        //////        method: 'POST',
+        //////        headers: {
+        //////            'Content-Type': 'application/json',
+        //////            'Content-Length': Buffer.byteLength(jsonObject, 'utf8')
+        //////        }
+        //////    };
+
+
+        //////    var req = https.request(options, (res) => {
+        //////        console.log('STATUS:', res.statusCode);
+        //////        console.log('HEADERS:', JSON.stringify(res.headers));
+        //////        res.setEncoding('utf8');
+        //////        res.on('data', (chunk) => {
+        //////            console.log('BODY:', chunk);
+        //////        });
+        //////        res.on('end', () => {
+        //////            console.log('No more data in response.');
+        //////        });
+        //////    });
+
+        //////    req.on('error', (e) => {
+        //////        console.log(`problem with request: ${e.message}`);
+        //////    });
+
+        //////    // write data to request body
+        //////    req.write(jsonObject);
+        //////    req.end();
+        //////} catch (err) {
+        //////    console.log("HEROKU INVOKE ERROR: " + err.message);
+        //////}
+
+
+
     }, 1 * 60 * 1000); // load every 20 minutes
 }
 startKeepAlive();
