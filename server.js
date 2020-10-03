ab91048e2e2ffeb57143548419d4033a56da71e2
caicangcua <36462470+caicangcua@users.noreply.github.com>
2020-10-03 08:31:29 +0700

    vc

--- server.js
+++ server.js
@@ -297,7 +297,6 @@ app.use(function (req, res) {
 //app.engine('html', require('ejs').renderFile);
 //app.set('view engine', 'html');
 app.set('view engine', 'ejs');
-///app.engine('ejs', require('ejs').__express);
 //
 const socket = require("socket.io");
 const server = app.listen(port, function () {
