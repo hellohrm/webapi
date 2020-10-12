4e5b32b3aed7c98c25a48c6ce1a1292badf7b685
caicangcua <36462470+caicangcua@users.noreply.github.com>
2020-10-12 19:33:07 +0700

    v4

--- server.js
+++ server.js
@@ -287,6 +287,32 @@ routes(app)
 
 app.route('/phphostprocessing').post(function (req, res) {
     var post_body = req.body;
+
+    var crypto = require('crypto');
+
+    var encrypt = function (plain_text, encryptionMethod, secret, iv) {
+        var encryptor = crypto.createCipheriv(encryptionMethod, secret, iv);
+        return encryptor.update(plain_text, 'utf8', 'base64') + encryptor.final('base64');
+    };
+
+    var decrypt = function (encryptedMessage, encryptionMethod, secret, iv) {
+        var decryptor = crypto.createDecipheriv(encryptionMethod, secret, iv);
+        return decryptor.update(encryptedMessage, 'base64', 'utf8') + decryptor.final('utf8');
+    };
+
+    var textToEncrypt = new Date().toISOString().substr(0, 19) + '|My super secret information.';
+    var encryptionMethod = 'AES-256-CBC';
+    var secret = "My32charPasswordAndInitVectorStr"; //must be 32 char length
+    var iv = secret.substr(0, 16);
+
+    var encryptedMessage = encrypt(textToEncrypt, encryptionMethod, secret, iv);
+    var decryptedMessage = decrypt(encryptedMessage, encryptionMethod, secret, iv);
+    var decryptedMessage = decrypt(decodeURIComponent(post_body['token']), encryptionMethod, secret, iv);
+
+    console.log(encryptedMessage);
+    console.log(decryptedMessage);
+
+
     io.emit('announcements', { message: JSON.stringify( post_body) });
 });
 
