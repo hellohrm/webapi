c028b2bbd61fb341fb386ac6b90bac03b7305997
caicangcua <36462470+caicangcua@users.noreply.github.com>
2022-01-29 18:56:50 +0700

    v3

--- server.js
+++ server.js
@@ -20,108 +20,108 @@ const pushsrv = net.createServer((c) => {
 pushsrv.on('error', (err) => {
     throw err;
 });
-pushsrv.listen(8124, () => {
+pushsrv.listen(3000, () => {
     console.log('server bound');
 });
 
 
 
-var app = express();
-var server = http.createServer(app);
+//var app = express();
+//var server = http.createServer(app);
 
 
-var path = require('path');
-app.use(express.static(path.join(__dirname, './chat')));
-app.get('/chat', (req, res) => {
-    //debugger;
-    console.log(req.headers);
-    res.sendFile(__dirname + '/chat/index.html');
-}).post('/chat', (req, res) => {
-    //debugger;
-    // push the data to body
-    // console.log(req.headers);
-    var body = [];
-    req.on('data', (chunk) => {
-        body.push(chunk);
-    }).on('end', () => {
-        // on end of data, perform necessary action
-        body = Buffer.concat(body).toString();
-        //response.write(request.body.user);
-        //response.end();
-        console.log(body);
-    });
-});
-
-
-
+//var path = require('path');
+//app.use(express.static(path.join(__dirname, './chat')));
+//app.get('/chat', (req, res) => {
+//    //debugger;
+//    console.log(req.headers);
+//    res.sendFile(__dirname + '/chat/index.html');
+//}).post('/chat', (req, res) => {
+//    //debugger;
+//    // push the data to body
+//    // console.log(req.headers);
+//    var body = [];
+//    req.on('data', (chunk) => {
+//        body.push(chunk);
+//    }).on('end', () => {
+//        // on end of data, perform necessary action
+//        body = Buffer.concat(body).toString();
+//        //response.write(request.body.user);
+//        //response.end();
+//        console.log(body);
+//    });
+//});
 
 
 
-var name;
-
-const port = process.env.PORT || 3000
-
-const socket = require("socket.io");
-
-
-//Socket setup
-const io = socket(server, { origin: "*:*" });
-
-io.on("connection", function (socket) {
-
-    console.log('new user connected');
-
-    socket.on('joining msg', (username) => {
-        name = username;
-        io.emit('chat message', `---${name} joined the chat---`);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        io.emit('chat message', `---${name} left the chat---`);
-
-    });
-    socket.on('chat message', (msg) => {
-        socket.broadcast.emit('chat message', msg);         //sending message to all except the sender
-    });
-});
-
-
-let routes = require('./api/routes'); //importing route
-routes(app, {
-    testsend:function (data) {
-        io.emit('chat message', data);
-    }
-})
-
-//const request = require('request');
-app.use(function (req, res) {
-
-    //res.status(404).send({ url: req.originalUrl + ' not found' })
-    //res.status(301).redirect('https://hrpro.cf/pages/invoice.html' + req.url) // 'http://192.168.1.91:10996/pages/testredirect.php?XDEBUG_SESSION_START=154A5348'
-    //console.log(req.body);
-    //res.writeHead(301,
-    //    { Location: 'http://192.168.1.91:10996/localsrc/ping.php?XDEBUG_SESSION_START=154A5348',fuck:'dumemay' }////'http://192.168.1.91:10996/pages/invoice.html'
-    //);
-    //redirect_post( req.body);
-
-    //var url = req.originalUrl;//'http://192.168.1.91:10996/te.php?XDEBUG_SESSION_START=154A5348' ;//'YOUR_API_BASE_URL' + req.url;
-    //var r = null;
-    //if (req.method === 'POST') {
-    //    r = request.post({ uri: url, json: req.body });
-    //} else {
-    //    r = request(url);
-    //}
-
-    //req.pipe(r).pipe(res);
-
-
-    res.end();
-})
 
 
 
-server.listen(port, function () {
-    console.log(`Listening on port ${port}`);
-    console.log(`http://localhost:${port}`);
-});
+//var name;
+
+//const port = process.env.PORT || 3000
+
+//const socket = require("socket.io");
+
+
+////Socket setup
+//const io = socket(server, { origin: "*:*" });
+
+//io.on("connection", function (socket) {
+
+//    console.log('new user connected');
+
+//    socket.on('joining msg', (username) => {
+//        name = username;
+//        io.emit('chat message', `---${name} joined the chat---`);
+//    });
+
+//    socket.on('disconnect', () => {
+//        console.log('user disconnected');
+//        io.emit('chat message', `---${name} left the chat---`);
+
+//    });
+//    socket.on('chat message', (msg) => {
+//        socket.broadcast.emit('chat message', msg);         //sending message to all except the sender
+//    });
+//});
+
+
+//let routes = require('./api/routes'); //importing route
+//routes(app, {
+//    testsend:function (data) {
+//        io.emit('chat message', data);
+//    }
+//})
+
+////const request = require('request');
+//app.use(function (req, res) {
+
+//    //res.status(404).send({ url: req.originalUrl + ' not found' })
+//    //res.status(301).redirect('https://hrpro.cf/pages/invoice.html' + req.url) // 'http://192.168.1.91:10996/pages/testredirect.php?XDEBUG_SESSION_START=154A5348'
+//    //console.log(req.body);
+//    //res.writeHead(301,
+//    //    { Location: 'http://192.168.1.91:10996/localsrc/ping.php?XDEBUG_SESSION_START=154A5348',fuck:'dumemay' }////'http://192.168.1.91:10996/pages/invoice.html'
+//    //);
+//    //redirect_post( req.body);
+
+//    //var url = req.originalUrl;//'http://192.168.1.91:10996/te.php?XDEBUG_SESSION_START=154A5348' ;//'YOUR_API_BASE_URL' + req.url;
+//    //var r = null;
+//    //if (req.method === 'POST') {
+//    //    r = request.post({ uri: url, json: req.body });
+//    //} else {
+//    //    r = request(url);
+//    //}
+
+//    //req.pipe(r).pipe(res);
+
+
+//    res.end();
+//})
+
+
+
+//server.listen(port, function () {
+//    console.log(`Listening on port ${port}`);
+//    console.log(`http://localhost:${port}`);
+//});
